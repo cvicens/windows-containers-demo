ee6c2440d7a689316794e49e6e15e9f2abfeac99
cvicens <carlos.vicens.alonso@gmail.com>
2021-12-09 15:37:25 +0100

    test 1

--- samples/aspnetapp/aspnetapp/About.aspx.cs
+++ samples/aspnetapp/aspnetapp/About.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Http;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,34 @@ namespace aspnetapp
 {
     public partial class About : Page
     {
+        private string _host = "localhost";
+        private int _port = 5002;
+        private string _uri = "WeatherForecast";
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected About GetAbout()
+        {
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri("http://" + _host + ":" + _port + "/");
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            client.DefaultRequestHeaders.Add("User-Agent", "HttpClientFactory-Sample");
+
+            var response = await client.GetAsync(_uri);
+
+            if (!response.IsSuccessStatusCode) {
+                _logger.LogError("Problem when calling client service code: " + response.StatusCode);
+            }
+
+            response.EnsureSuccessStatusCode();
+            
+            using var responseStream = await response.Content.ReadAsStreamAsync(); 
+           
+            var weatherForecasts = await JsonSerializer.DeserializeAsync<WeatherForecast[]>(responseStream, options);
+            return weatherForecasts;
         }
     }
 }
