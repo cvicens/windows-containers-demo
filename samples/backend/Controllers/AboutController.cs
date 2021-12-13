f0569e6aee476a8b19942e3cced3ab0513574009
cvicens <carlos.vicens.alonso@gmail.com>
2021-12-13 09:31:46 +0100

    cdummy data added

--- samples/backend/Controllers/AboutController.cs
+++ samples/backend/Controllers/AboutController.cs
@@ -13,17 +13,17 @@ namespace Backend.Controllers
     {
         private static readonly string[] Titles = new[]
         {
-            "About", "This is the about"
+            "About from .NET Core", "This is another about from .NET Core"
         };
 
         private static readonly string[] Descriptions = new[]
         {
-            "Description 1", "Awesome description"
+            "This is a sample description from a sample .NET Core.", "Awesome description from a no less awesome .NET Core simple service."
         };
 
         private static readonly string[] Details = new[]
         {
-            "Awesome details", "Lorem ipsum...", "Howdy blah blah"
+            "Irrelevant details just to fill in a screen... from .NET CORE", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent consequat faucibus nunc id lobortis. Etiam ut vehicula magna, sed molestie quam. Aliquam vestibulum aliquam nisi, quis faucibus urna. Maecenas libero nulla, iaculis sed pellentesque ac, cursus ut elit. Proin pharetra dui in turpis scelerisque varius. Vestibulum vel tortor et urna fermentum ultricies. Suspendisse lobortis elit est, in pellentesque nunc dapibus sed. Donec at risus eget mauris pellentesque convallis id in justo. Pellentesque pretium metus enim, ut condimentum felis dictum quis.", "The devil is in the detail."
         };
 
         private readonly ILogger<AboutController> _logger;
